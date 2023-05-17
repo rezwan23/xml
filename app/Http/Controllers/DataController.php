a948e1e9a6a6899468514148f7d63d41407f1493
Md Razwanul Ghani <razwanulghani@gmail.com>
2023-05-17 18:31:27 +0600

    initial commit

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Http;
+
+class DataController extends Controller
+{
+    public function getData()
+    {
+        // $response = Http::get('http://webserver.flak.no/vbilder/FlakXMLStockBalance.xml');
+
+        // $new = simplexml_load_string($response->body());
+  
+        // Convert into json
+        // $con = json_encode($new);
+        
+        // Convert into associative array
+        // $newArr = json_decode($con, true);
+
+        // dd($newArr['StockBalances']['StockBalance']);
+
+        // return json_encode($newArr['StockBalances']['StockBalance']);
+
+        $arr = [
+            [
+                'ProductNo' => 1036922,
+                'Description' => "Emma Hedström",
+                "SRP" => 109.000000,
+                "StockBalance" => 20
+            ]
+        ];
+
+
+        return json_encode($arr);
+
+
+    }
+}
